b7acb1753df881c9ac0026b146ecd39d9d4060ed
Javier Huertas <josephcarlos.jcmn@gmail.com>
2022-10-20 07:23:28 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/TipoHabitacionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionMVCConCapas.Controllers
+{
+    public class TipoHabitacionController : Controller
+    {
+        // GET: TipoHabitacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+        public ActionResult VistaPruebaInicio()
+        {
+            return View();
+        }
+    }
+}
